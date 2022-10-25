e630354d070ea78a4f3ec751d84fd4bcbf69974b
NUM1NEX <g.koszyk98@gmail.com>
2022-10-25 10:26:54 +0200

    Added generic repository

---
+++ Wixapol_DataAccess/GenericRepository/Implementation/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wixapol_DataAccess.GenericRepository.Implementation
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> GetAll(string storedProcedure, string connectionStringName);
+        void SaveData(string storedProcedure, T parameters, string connectionStringName);
+        void RemoveData(string storedProcedure, int id, string connectionStringName);
+    }
+}
