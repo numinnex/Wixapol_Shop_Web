b1a43fcefb520a6ddf2074ef96fa1d473786e729
NUM1NEX <g.koszyk98@gmail.com>
2022-10-26 15:46:32 +0200

    Refactor Create view

---
+++ WixapolShop/Areas/Admin/Controllers/ProducentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wixapol_DataAccess.UnitOfWork.Interface;
+
+namespace WixapolShop.Areas.Admin.Controllers
+{
+    public class ProducentController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProducentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wixapol_DataAccess/Models/Producent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wixapol_DataAccess.Models
+{
+    public class Producent
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ProducentCode { get; set; }
+        [Required]
+        public string EAN { get; set; }
+    }
+}
